ece94ef7341b4b918c73628e6335fe756b344dea
Justin Guertin <justin.guertin@gmail.com>
2019-07-28 21:17:54 -0700

    Incorporate Firebase into the project

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <q-toolbar class="header bg-primary text-white">
       <q-toolbar-title><router-link to="/">Vituary Solutions</router-link></q-toolbar-title>
 
-      <q-btn flat round icon="fas fa-envelope" type="a" href="mailto:jguertin@vituary.com"><q-tooltip>Email</q-tooltip></q-btn>
       <q-btn flat round icon="fab fa-github" type="a" href="https://github.com/jguertin"><q-tooltip>Github</q-tooltip></q-btn>
       <q-btn flat round icon="fab fa-linkedin" type="a" href="https://www.linkedin.com/in/justinguertin"><q-tooltip>LinkedIn</q-tooltip></q-btn>
       <q-btn flat round icon="fab fa-twitter" type="a" href="https://twitter.com/jguertin"><q-tooltip>Twitter</q-tooltip></q-btn>
